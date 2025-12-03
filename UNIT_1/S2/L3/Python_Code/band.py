520df479790216d7380ea959a12b8cff24399e33
Amin23ElKassimi <aminelkassimi01@gmail.com>
2025-12-03 16:04:27 +0100

    Pratica + Esercizi Bonus

---
+++ UNIT_1/S2/L3/Python_Code/band.py
@@ -0,0 +1,13 @@
+# Programma per generare un nome di una band
+
+# Chiedi all'utente il nome della città
+citta = input("Inserisci il nome della tua città di origine: ")
+
+# Chiedi all'utente il nome del proprio animale domestico
+animale = input("Inserisci il nome del tuo animale domestico: ")
+
+# Genera il nome della band combinando i due input
+nome_band = citta + " " + animale
+
+# Stampa il risultato
+print("Il nome della tua band potrebbe essere:", nome_band)
