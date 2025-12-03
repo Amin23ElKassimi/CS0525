cbb38e29abf74976f331592ae37f014cf2fbdb51
Amin <aminelkassimi01@gmail.com>
2025-12-03 12:36:35 -0500

    Clean String

--- UNIT_1/S2/L3/Python_Code/Ex2.py
+++ UNIT_1/S2/L3/Python_Code/Ex2.py
@@ -1,1 +1,10 @@
-print("Hello World")
+def remove_punctuation(text):
+    result = ""
+    for char in text:
+        if char.isalpha() or char.isspace():
+            result += char
+    return result.lower()
+
+sample_text = "Hello, world! Python is fun; isn't it?"
+cleaned_text = remove_punctuation(sample_text)
+print(cleaned_text)
