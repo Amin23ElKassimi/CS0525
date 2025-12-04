17f2a4873a1c419a41095b596665a83dc4315be4
Amin23ElKassimi <aminelkassimi01@gmail.com>
2025-12-04 17:55:18 +0100

    Refactor usando "struttura condizionale multipla"

--- UNIT_1/S2/L4/04_calculatorperimeter.py
+++ UNIT_1/S2/L4/04_calculatorperimeter.py
@@ -8,24 +8,25 @@
 import math
 
 #Scegliere la figura geometrica
-input_figure = input("Scegli la figura geometrica (quadrato, cerchio, rettangolo): ").lower()
+input_figure = int(input("Scegli la figura geometrica per calcolare il perimetro:\n1. Quadrato\n2. Cerchio\n3. Rettangolo\nInserisci il numero corrispondente alla tua scelta: "))
 
 # Funzione per calcolare il perimetro del quadrato
-if input_figure == "quadrato":
+if input_figure == 1:
     lato = float(input("Inserisci la lunghezza del lato del quadrato: "))
     perimetro_quadrato = lato * 4
     print(f"Il perimetro del quadrato è: {perimetro_quadrato}")
 
-
-# Funzione per calcolare il perimetro del cerchio
-elif input_figure == "cerchio":
+# Funzione per calcolare la circonferenza del cerchio
+elif input_figure == 2:
     raggio = float(input("Inserisci il raggio del cerchio: "))
-    perimetro_cerchio = 2 * math.pi * raggio
-    print(f"La circonferenza del cerchio è: {perimetro_cerchio}")
+    circonferenza_cerchio = 2 * math.pi * raggio
+    print(f"La circonferenza del cerchio è: {circonferenza_cerchio}")
 
-# Funzione per calcolare il perimetro del rettangolo
-elif input_figure == "rettangolo":
+#Funzione per calcolare il perimetro del rettangolo
+elif input_figure == 3:
     base = float(input("Inserisci la lunghezza della base del rettangolo: "))
     altezza = float(input("Inserisci l'altezza del rettangolo: "))
-    perimetro_rettangolo = 2 * (base + altezza) # ho corretto la formula del perimetro del rettangolo il prodotto lo eseguo dopo
+    perimetro_rettangolo = 2 * (base + altezza)
     print(f"Il perimetro del rettangolo è: {perimetro_rettangolo}")
+
+# Questo codice usa il metodo che in programmazione viene chiamato "struttura condizionale multipla" per eseguire diverse sezioni di codice in base alla scelta dell'utente.
